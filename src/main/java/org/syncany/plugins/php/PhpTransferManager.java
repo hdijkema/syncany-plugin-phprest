12d52bd31c17a66584e13e87e72ca39d24a55c9b
hdijkema <hans@dykema.nl>
2015-01-20 14:42:41 +0100

    Finally the right stuff

--- src/main/java/org/syncany/plugins/php/PhpTransferManager.java
+++ src/main/java/org/syncany/plugins/php/PhpTransferManager.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.syncany.plugins.phprest;
+package org.syncany.plugins.php;
 
 import java.io.File;
 import java.util.HashMap;
--- src/main/java/org/syncany/plugins/php/PhpTransferPlugin.java
+++ src/main/java/org/syncany/plugins/php/PhpTransferPlugin.java
@@ -15,12 +15,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.syncany.plugins.phprest;
+package org.syncany.plugins.php;
 
 import org.syncany.plugins.transfer.TransferPlugin;
 
 public class PhpTransferPlugin extends TransferPlugin {
 	public PhpTransferPlugin() {
-		super("phprest");
+		super("php");
 	}
 }
